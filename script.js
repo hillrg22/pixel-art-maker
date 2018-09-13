7293c1f92df8d1d08d04a76384bebfe61839e99b
hillrg22 <hillrg22@gmail.com>
2018-09-13 16:33:45 -0600

    adjusted colorSelect function, got basic functionality for pixel art maker

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 var container = document.querySelector('.container')
 var pixel = document.querySelector('.pixel')
 var currentColor = ''
+var palette = document.querySelector('.palette')
+var colorPicks = ['red', 'blue', 'orange' , 'yellow', 'green', 'purple']
+
+//create divs w/ color options
+for(let i=0; i< colorPicks.length; i++){
+  let div = document.createElement('div')
+  palette.appendChild(div)
+  div.classList= "";
+  div.classList.add("pixel");
+  div.classList.add(colorPicks[i]);
+  console.log("color picks", colorPicks[i])
+}
+
+//select currentColor
+palette.addEventListener('click', colorSelect)
+
+function colorSelect(event){
+  currentColor = event.target.classList[1]
+  console.log("Class name", event.target.classList[1], currentColor)
+}
+
+
 
 //select pixel
 function clickPixel(event){
   console.log(event.target)
-  event.target.style.backgroundColor = 'red'
+  console.log(event)
+  event.target.style.backgroundColor = currentColor
 }
 container.addEventListener('click', clickPixel)
-
-//select currentColor
-
-function colorSelect(event){
-  
-}
