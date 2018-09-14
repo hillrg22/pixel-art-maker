6ced870dc32b08a399512464daae9f796b1c5c1d
hillrg22 <hillrg22@gmail.com>
2018-09-14 10:48:33 -0600

    removed hard coded divs for canvas in html, and created for loop to create them through script file. Improved positioning on page for canvas and pallette

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ var currentColor = ''
 var palette = document.querySelector('.palette')
 var colorPicks = ['red', 'blue', 'orange' , 'yellow', 'green', 'purple', 'brown', 'grey', 'black', 'white']
 
-//create divs w/ color options
+//create divs w/ color options (palette)
 for(let i=0; i< colorPicks.length; i++){
   let div = document.createElement('div')
-  palette.appendChild(div)
+  palette.appendChild(div);
   div.classList= "";
   div.classList.add("pixel");
   div.classList.add(colorPicks[i]);
   // console.log("color picks", colorPicks[i])
 }
 
+//create divs for canvas
+for(let i=0; i<400; i++){
+  let div = document.createElement("div")
+  container.appendChild(div);
+  div.classList = "";
+  div.classList.add("pixel");
+
+}
+
+
 //select currentColor
 palette.addEventListener('click', colorSelect)
 
 function colorSelect(event){
   currentColor = event.target.classList[1]
+  // console.log(event.target)
   // console.log("Class name", event.target.classList[1], currentColor)
 }
 
