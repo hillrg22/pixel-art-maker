6009864bc3736f90be7d07603351110b76f79919
hillrg22 <hillrg22@gmail.com>
2018-12-07 15:53:37 -0700

    cleaned up code, removed comments

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var currentColor = ''
 var palette = document.querySelector('.palette')
 var colorPicks = ['red', 'blue', 'orange' , 'yellow', 'green', 'purple', 'brown', 'grey', 'black', 'white']
 
-//create divs w/ color options (palette)
 for(let i=0; i< colorPicks.length; i++){
   let div = document.createElement('div')
   palette.appendChild(div);
@@ -13,29 +12,21 @@ for(let i=0; i< colorPicks.length; i++){
   div.classList.add(colorPicks[i]);
 }
 
-//create divs for canvas
-
   for(let i=0; i<405; i++){
     let div = document.createElement('div')
     container.appendChild(div);
     div.classList = '';
     div.classList.add('pixel');
-    div.addEventListener('click', clickPixel)
+    div.addEventListener('click', clickPixel);
   }
 
-
-
-
-//select currentColor
 palette.addEventListener('click', colorSelect)
 
 function colorSelect(event){
-  currentColor = event.target.classList[1]
+  currentColor = event.target.classList[1];
 
 }
 
-//color pixel on the canvas
-
 function clickPixel(event){
-  event.target.style.backgroundColor = currentColor
+  event.target.style.backgroundColor = currentColor;
 }
